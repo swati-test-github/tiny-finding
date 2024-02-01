edf33b20944ef076e6bf19cb431b512af71e09d8
David Whitlow <david.whitlow@r2c.dev>
2024-02-01 12:22:18 -0800

    add insecure grpc2

---
+++ insecure-grpc2.go
@@ -0,0 +1,22 @@
+package insecuregrpc
+
+import (
+	"crypto/x509"
+	"log"
+	"net/http"
+	"net/http/httptest"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+// cf. https://blog.gopheracademy.com/advent-2019/go-grps-and-tls/#connection-without-encryption
+func unsafe() {
+	// Server
+	// ruleid:grpc-server-insecure-connection
+	s := grpc.NewServer()
+	// ... register gRPC services ...
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
