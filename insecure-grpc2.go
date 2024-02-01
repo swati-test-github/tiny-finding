5ab7b243c8de823a6ff4ff38c06ffb13f9c4e869
David Whitlow <david.whitlow@r2c.dev>
2024-02-01 12:47:37 -0800

    fix vuln in grpc2

--- insecure-grpc2.go
+++ insecure-grpc2.go
@@ -13,8 +13,8 @@ import (
 // cf. https://blog.gopheracademy.com/advent-2019/go-grps-and-tls/#connection-without-encryption
 func unsafe() {
 	// Server
-	// ruleid:grpc-server-insecure-connection
-	s := grpc.NewServer()
+
+
 	// ... register gRPC services ...
 	if err = s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
